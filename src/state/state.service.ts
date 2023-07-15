c8f24c29ee5cb28b5669129bc2b4940e2dfaadcd
Mcgill Dias <mcgill.dias@gmail.com>
2023-07-15 04:08:20 -0300

    feat: add migrations address and inserts

---
+++ src/state/state.service.ts
@@ -0,0 +1,20 @@
+/*
+https://docs.nestjs.com/providers#services
+*/
+
+import { Injectable } from '@nestjs/common';
+import { StateEntity } from './entities/state.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class StateService {
+  constructor(
+    @InjectRepository(StateEntity)
+    private readonly stateRepository: Repository<StateEntity>,
+  ) {}
+
+  async getAllStates(): Promise<StateEntity[]> {
+    return this.stateRepository.find();
+  }
+}
